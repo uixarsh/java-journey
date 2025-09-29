27b2a67cea9422ca32595ccbd5ffee1066057e96
Arsh Singhal <singhalarsh2001@gmail.com>
2025-09-29 13:19:21 +0000

    Fixing Lecture structures

--- Lesson5/src/Basic.java
+++ Lesson5/src/Basic.java
@@ -1,7 +1,18 @@
 import java.util.Arrays;
 
 public class Basic {
+    /*
+    * Array
+    * Collection of Data types.
+    * be its primitive, objects etc.
+    * Homogeous Data Structure.
+    */
     public static void main(String[] args) throws Exception {
+        /*
+         * Syntax
+         * data_type [] variablename = new data_type (size);
+        */
+
         int[] roll_no = new int[5];
         // System.out.println(roll_no[1]);
         System.out.println(Arrays.toString(roll_no));
